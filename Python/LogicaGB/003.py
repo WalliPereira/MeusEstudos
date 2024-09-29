752bbbf6576f1c16056c8d79c00ea47d7e3c4615
Wallisson Pereira <157427235+WalliPereira@users.noreply.github.com>
2024-09-29 11:05:34 -0300

    Update 003.py

--- Python/LogicaGB/003.py
+++ Python/LogicaGB/003.py
@@ -1,7 +1,7 @@
-#Informe ao usuário as quantidades de tinta a serem compradas e os respectivos preços em 3 situações:
-#comprar apenas latas de 18 litros;
-#comprar apenas galões de 3,6 litros;
-#misturar latas e galões, de forma que o desperdício de tinta seja menor. Acrescente 10% de folga e sempre arredonde os valores para cima, isto é, considere latas cheias.
+1.Informe ao usuário as quantidades de tinta a serem compradas e os respectivos preços em 3 situações:
+  comprar apenas latas de 18 litros;
+  comprar apenas galões de 3,6 litros;
+  misturar latas e galões, de forma que o desperdício de tinta seja menor. Acrescente 10% de folga e sempre arredonde os valores para cima, isto é, considere latas cheias.
 
 import math
 

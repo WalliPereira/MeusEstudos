7330d9a6dbd7e25e16eed1599e406bfe1d2cc699
Wallisson Pereira <157427235+WalliPereira@users.noreply.github.com>
2024-10-05 12:39:36 -0300

    Add files via upload

---
+++ Python/padrao/orientacaobj/app.py
@@ -0,0 +1,13 @@
+from classes import vendedor
+
+vendedor1 = vendedor("Wallisson")
+vendedor1.vendeu(1000)
+vendedor1.bateuMeta(600)
+
+vendedor2 = vendedor("Joaquim")
+vendedor2.vendeu(300)
+vendedor2.bateuMeta(600)
+
+vendedor3 = vendedor("Gabriel")
+vendedor3.vendeu(2256)
+vendedor3.bateuMeta(600)
